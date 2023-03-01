87bffc9b60ee12f24bb2976eda36a238ead42520
gvdw-cis <52698009+gvdw-cis@users.noreply.github.com>
2023-03-01 16:48:28 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,140 +1,65 @@
-//Create a azure Resource Group
-variable "resourcegroup" {
-  description = "Name of the azure ResourceGroup"
-  default     = "pm247-stage-resources"
+variable "azurerm_resource_group_name" {
+  type        = string
+  description = "Enter the resource group name"
+  default     = "terraform-database-resource-group"
+}
+variable "azurerm_network_security_group_name" {
+  type        = string
+  description = "Enter the security group name"
+  default     = "mi-security-group-terraform"
+}
+variable "azurerm_virtual_network_name" {
+  type        = string
+  description = "Enter the virtual network name"
+  default     = "vnet-mi-terraform"
+}
+variable "azurerm_subnet_name" {
+  type        = string
+  description = "Enter subnet name"
+  default     = "subnet-mi-terraform"
 }
 variable "location" {
-  description = "location for all the resources"
-  default     = "West Europe"
+  type        = string
+  description = "Enter the location where you want to deploy the resources"
+  default     = "eastus"
 }
 
-//The following section creates a virtual network in the 10.0.0.0/16 address space:
-variable "virtnetname" {
-  description = "name of the virtualNetwork"
-  default     = "xVirtualNetwork"
-}
-variable "vnet_addr_space" {
-  description = "range of the ip's for virt. network address space"
-  default = "10.0.0.0/16"
+variable "administrator_login" {
+  type        = string
+  description = "Enter Administrator name for the database"
+  default     = "VeryStrongAdministrator"
 }
 
-//The following section creates a subnet in the above virtual network:
-variable "subnetname" {
-  description = "Name of the SubnetMask"
-  default     = "xSubnet"
+variable "administrator_login_password" {
+  type        = string
+  description = "Enter administrator password for the database"
+  default     = "IamAVeryStrongP@ssw0rd123"
+  sensitive   = true
 }
 
-variable "subnet_addr_prefix" {
-  description = "subnet address space"
-  default = "10.0.2.0/24"
+variable "database_name" {
+  type        = string
+  description = "Enter database name"
+  default     = "sql-mi-terraform"
 }
 
-// Create public IPs
-variable "publicip" {
-  description = "Public Ip of the VM"
-  default     = "sqlPublicIP"
+variable "sku_name" {
+  type        = string
+  description = "Enter SKU"
+  default     = "GP_Gen5"
 }
-
-// Create Network Security Group and rule For Sql Server vm
-variable "sqlsecgrp" {
-  description = "Security Group name of the VM"
-  default     = "sql_sec_grp"
+variable "license_type" {
+  type        = string
+  description = "Enter license type"
+  default     = "BasePrice"
 }
-
-variable "sqlserver_port1_name" {
-  description = "name of the port"
-  default = "rdp"
+variable "vcores" {
+  type        = number
+  description = "Enter number of vCores you want to deploy"
+  default     = 8
 }
-
-variable "sql_port_number1" {
-  description = "rdp port"
-  default = "3389"
-}
-
-variable "sqlserver_port2_name" {
-  description = "name of the port2"
-  default = "sql"
-}
-
-variable "sql_port_number2" {
-  description = "sql server port"
-  default = "1433"
-}
-
-//This creates a virtual NIC named acctNIC connected to the virtual networking resources we have created:
-variable "sqlnicnew" {
-  description = "sqlvm network interface card"
-  default = "sqlnicnew"
-}
-
-variable "sqlvmipnew-configuration" {
-  description = "name of the sqlvmipnew-configuration"
-  default = "sqlvmipnew-configuration"
-}
-
-//The following section creates a VM and attaches the virtual NIC to it.
-
-variable "sqlvmname" {
-  description = "sqlvmname"
-  default = "sqlvminstancenew"
-}
-variable "vm_size" {
-  description = "vm_size"
-  default = "Standard_D4s_v3"
-}
-
-//Find the VM images in the Azure Marketplace with the Azure CLI tool
-variable "i_offer" {
-  description = "offer for the vm"
-  default = "SQL2016SP1-WS2016"
-}
-variable "i_publisher" {
-  description = "Publisher for the  vm"
-  default = "MicrosoftSQLServer"
-}
-
-variable "i_sku" {
-  description = "sku for the vm"
-  default = "SQLDEV"
-}
-variable "i_version" {
-  description = "version for the vm"
-  default = "latest"
-}
-
-//boot diagnosetic: 
-variable "boot_url" {
-  description = "boot_url"
-  default = "https://xxxxxx.blob.core.windows.net"
-}
-
-//Windows OS disk by default it is of 128 GB
-variable "os_disk" {
-  description = "os_disk"
-  default = "sqlnewOsDisk"
-}
-// Adding additional disk for persistent storage
-variable "add_disk_name" {
-  description = "additional_disk"
-  default = "sqlvm_disk_1"
-}
-variable "add_disk_size" {
-  description = "disk size in GB"
-  default = "128"
-}
-
-//Here defined admin uid/pwd and also comupter name
-variable "computer_name" {
-  description = "name"
-  default = "sqlxvm"
-}
-
-variable "admin_username" {
-  description = "name"
-  default = "myxadmin"
-}
-
-variable "admin_password" {
-  description = "passwd"
-  default = "213rdssword"
+variable "storage_size_in_gb" {
+  type        = number
+  description = "Enter storage size in GB"
+  default     = 32
 }
